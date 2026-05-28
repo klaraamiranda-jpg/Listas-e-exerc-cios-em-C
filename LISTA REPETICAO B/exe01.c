13f1cdca312c321f78e3c3eeb0b1c88f3a4d943f
Anna Klara Miranda <klaraamiranda@gmail.com>
2026-05-28 12:03:22 -0300

    Delete LISTA REPETICAO B directory

--- LISTA REPETICAO B/exe01.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main(){
-	
-	int i;
-	for(i=1;i<=10;i++){
-		printf("%d ",i);
-	}
-}
-
--- LISTA REPETICAO B/exe02.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main(){
-	
-	int i=1;
-	while(i<=10){
-		printf("%d ",i);
-		i++;
-	}
-}
